f1b67711321660724b8a234ac9a810d3e2cdefdd
sushmitaa07 <sushmitapandey881@gmail.com>
2025-04-21 18:30:34 +0545

    initial commit

--- app/src/main/java/com/example/myprojectt/LoginActivity.kt
+++ app/src/main/java/com/example/myprojectt/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -18,30 +19,18 @@ class LoginActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyProjecttTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold { innerpadding->
+                Column(
+                    modifier =
+                        Modifier
+                            .padding(innerpadding)
+                            .fillMaxSize()
+                            ..background(color = color.green)
+                )
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    MyProjecttTheme {
-        Greeting2("Android")
-    }
-}
